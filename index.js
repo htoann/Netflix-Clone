2d4661fcff11b7235bc00d5cc64d4647db1e1b57
Trần Hữu Toàn <jungleoffice02@gmail.com>
2022-04-02 21:51:46 +0700

    Test

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, () => {
   console.log("Backend server is running...");
 });
