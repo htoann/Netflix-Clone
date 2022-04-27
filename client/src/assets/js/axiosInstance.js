e0e1cd9d6ff09763b6b69dcf4516f6646da6df13
Trần Hữu Toàn <jungleoffice02@gmail.com>
2022-04-27 20:47:00 +0700

    make it better

--- client/src/assets/js/axiosInstance.js
+++ client/src/assets/js/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://netflixtht.herokuapp.com/api/",
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://netflixtht.herokuapp.com/api/",
+  // baseURL: "http://localhost:8800/api/",
 });
