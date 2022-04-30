7fe9cb9dd3029571260b057736681129012247fd
Trần Hữu Toàn <jungleoffice02@gmail.com>
2022-04-30 20:10:25 +0700

    Fix

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -31,35 +31,33 @@ function ListItem({ item }) {
   }, [item]);
 
   return (
-    <div>
-      <div className="listItem" onClick={() => setIsClicked(!isClicked)}>
-        <img className="listItem__img" src={movie.img} alt="" />
-        {isClicked && (
-          <div className={isClicked ? "listItem__active" : ""}>
-            <div className="itemInfo">
-              <h1 className="listItem__active_title">{movie.title}</h1>
-              <div className="icons">
-                <Link to={{ pathname: "/watch" }} state={{ movie: movie }}>
-                  <PlayCircleFilledRounded className="icon" />
-                </Link>
-                <AddCircleOutlineRounded className="icon" />
-                <ThumbUpAltOutlined className="icon" />
-                <ThumbDownOutlined className="icon" />
-              </div>
-              <div className="itemInfoTop py-1">
-                <span className="text-green-500 text-xs">98% Match</span>
-                <span className="border p-1">{movie.limit}</span>
-                <span>{movie.isSeries ? "Series" : "Limited"}</span>
-              </div>
-              <div className="itemInfoBottom pb-2">
-                <span className="listItem__itemInfoBottom_genre">
-                  {capitalizeFirstLetter(movie.genre)}
-                </span>
-              </div>
+    <div className="listItem" onClick={() => setIsClicked(!isClicked)}>
+      <img className="listItem__img" src={movie.img} alt="" />
+      {isClicked && (
+        <div className={isClicked ? "listItem__active" : ""}>
+          <div className="itemInfo">
+            <h1 className="listItem__active_title">{movie.title}</h1>
+            <div className="icons">
+              <Link to={{ pathname: "/watch" }} state={{ movie: movie }}>
+                <PlayCircleFilledRounded className="icon" />
+              </Link>
+              <AddCircleOutlineRounded className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop py-1">
+              <span className="text-green-500 text-xs">98% Match</span>
+              <span className="border p-1">{movie.limit}</span>
+              <span>{movie.isSeries ? "Series" : "Limited"}</span>
+            </div>
+            <div className="itemInfoBottom pb-2">
+              <span className="listItem__itemInfoBottom_genre">
+                {capitalizeFirstLetter(movie.genre)}
+              </span>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
