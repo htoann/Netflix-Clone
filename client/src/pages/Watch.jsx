dd12f6ebb3f9080733fba755723035e145fa26d9
Trần Hữu Toàn <jungleoffice02@gmail.com>
2022-05-24 12:17:53 +0700

    Fix siuuuuuu

--- client/src/pages/Watch.jsx
+++ client/src/pages/Watch.jsx
@@ -22,7 +22,7 @@ function Watch() {
         progress="true"
         controls
         muted
-        src="https://firebasestorage.googleapis.com/v0/b/netflix-16c38.appspot.com/o/items%2F1649760689070video?alt=media&token=e118fef6-59f9-448c-a323-bfa3e40c0203"
+        src="https://firebasestorage.googleapis.com/v0/b/netflix-16c38.appspot.com/o/items%2F1649760689070video?alt=media"
       ></video>
     </div>
   );
